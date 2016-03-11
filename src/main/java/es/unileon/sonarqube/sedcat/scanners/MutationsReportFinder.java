c6efeab0d797adef911dd333b2403ae57c5e7e09
alansastre <asastc00@estudiantes.unileon.es>
2016-03-11 01:04:54 +0100

    Pruebas de búsqueda en mutations finder.

--- src/main/java/es/unileon/sonarqube/sedcat/scanners/MutationsReportFinder.java
+++ src/main/java/es/unileon/sonarqube/sedcat/scanners/MutationsReportFinder.java
@@ -5,9 +5,14 @@ package es.unileon.sonarqube.sedcat.scanners;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.BatchSide;
 import org.sonar.api.internal.apachecommons.io.filefilter.FileFilterUtils;
 
@@ -19,27 +24,46 @@ import org.sonar.api.internal.apachecommons.io.filefilter.FileFilterUtils;
 @BatchSide
 public class MutationsReportFinder{
 	
-
+	private static final Logger LOG = LoggerFactory.getLogger(MutationsReportFinder.class);
+	
+	
 	public File findReport(File reportDirectory){
 		
+		
+		//TODO - DIRECTORIOS
+//		String[] directories = file.list(new FilenameFilter() {
+//			  @Override
+//			  public boolean accept(File current, String name) {
+//			    return new File(current, name).isDirectory();
+//			  }
+//			});
+
 		if (reportDirectory== null || !reportDirectory.exists() || !reportDirectory.isDirectory()) {
 		    return null;
 		}
-//		Collection<File> reports = FileUtils.listFiles(reportDirectory, new String[]{"index.html"}, true);
-		//TODO - arreglar
-		File[] reports = reportDirectory.listFiles(new FilenameFilter() {
-		    public boolean accept(File dir, String name) {
-		        return name.startsWith("index") && name.endsWith(".html");
-		    }
-		});
-
-		File latestReport = null;
-		for (File report : reports) {
-		  if (latestReport == null || FileUtils.isFileNewer(report, latestReport)) {
-		    latestReport = report;
-		  }
+		Collection<File> reports = FileUtils.listFiles(reportDirectory, null, false);
+		
+		if (reports!=null) {
+			LOG.info("Reportes encontrados: "+reports.size());
 		}
-		return latestReport;
+				File latestReport = null;
+		for (File report : reports) {
+			  if (latestReport == null || FileUtils.isFileNewer(report, latestReport)) {
+			    latestReport = report;
+			  }
+		}
+		
+		File indexReportSearched = null;
+		
+		Collection<File> indexReport = FileUtils.listFiles(latestReport, new String[]{"html"}, false);
+		
+		if (indexReport!=null) {
+			indexReportSearched= (File) indexReport.toArray()[0];
+			LOG.info("Reportes encontrados: "+indexReport.size());
+		}
+		
+		return indexReportSearched;
+
 
 	}
 	
