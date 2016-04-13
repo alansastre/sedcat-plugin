1eb01d91a83ce84eae8f28ff53e5ed5422c1e658
alansastre <asastc00@estudiantes.unileon.es>
2016-04-14 00:09:04 +0200

    Testing unitario: CoverageUnitTestsComputer

--- src/test/java/es/unileon/sonarqube/sedcat/start/AllTests.java
+++ src/test/java/es/unileon/sonarqube/sedcat/start/AllTests.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
 	
 	GeneralComputerTests.class,
-	SedcatPluginTests.class 
+	SedcatPluginTests.class,
+	CoverageUnitTestsComputerTests.class
 	
 })
 
