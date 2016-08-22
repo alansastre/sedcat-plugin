438596ca48f4c2a4faa0fa6d5c943e789e771d3d
alansastre <asastc00@estudiantes.unileon.es>
2016-08-22 18:25:34 +0200

    pruebas mocks cobertura

---
+++ src/test/java/org/sonar/plugins/sedcat/start/SedcatDashboardWidgetTests.java
@@ -0,0 +1,33 @@
+package org.sonar.plugins.sedcat.start;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.Assert;
+
+
+public class SedcatDashboardWidgetTests {
+
+	private SedcatDashboardWidget underTest;
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+
+		underTest = new SedcatDashboardWidget();
+		
+
+	}
+	@Test
+	public final void testGetters() {
+		Assert.assertNotNull(underTest.getId());
+		Assert.assertNotNull(underTest.getTemplate());
+		Assert.assertNotNull(underTest.getTitle());
+		Assert.assertNotNull(underTest.getTemplatePath());
+	}
+
+}
